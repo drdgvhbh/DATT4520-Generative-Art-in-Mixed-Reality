f54d901be4d0a50c8cf9307dc1dd32aef3209b96
Ryan Lee <drdgvhbh@gmail.com>
2019-10-28 19:48:10 -0400

    Add Jitter Chunk Header ID Types

---
+++ jitter-webserver/pkg/jitter/codec/header.go
@@ -0,0 +1,10 @@
+package codec
+
+// JitMatrixPacketID - Chunk ID of Jitter Chunk header
+const JitMatrixPacketID = "JMTX"
+
+// JitMatrixLatencyPacketID - Chunk ID of Jitter Chunk header
+const JitMatrixLatencyPacketID = "JMLP"
+
+// JitMessagePacketID - Chunk ID of Jitter Chunk header
+const JitMessagePacketID = "JMMP"
