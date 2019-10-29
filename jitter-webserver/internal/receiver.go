5f2c83cd4f787c454049665e540f60c975cbee4a
Ryan Lee <drdgvhbh@gmail.com>
2019-10-28 23:39:07 -0400

    Add loop for receiving jit matrix packets

--- jitter-webserver/internal/receiver.go
+++ jitter-webserver/internal/receiver.go
@@ -12,29 +12,30 @@ import (
 func Receive(c net.Conn) {
 	r := bufio.NewReader(c)
 
-	header, err := codec.DecodeJitNetPacketHeader(r)
-	if err != nil {
-		panic(err)
-	}
-
-	switch header.ID {
-	case codec.JitMatrixPacketID:
-		packetMatrix, err := codec.DecodeJitNetPacketMatrix(r)
+	for {
+		header, err := codec.DecodeJitNetPacketHeader(r)
 		if err != nil {
 			panic(err)
 		}
-		data := make([]byte, packetMatrix.Datasize)
-		_, err = io.ReadFull(r, data)
-		if err != nil {
-			panic(errors.Wrap(err, "failed to read data"))
-		}
-		// TODO: Handle
-	case codec.JitMatrixLatencyPacketID:
-		// TODO: Handle
-	case codec.JitMessagePacketID:
-		// TODO: Handle
-	default:
-		// TODO: Log Warning
-	}
 
+		switch header.ID {
+		case codec.JitMatrixPacketID:
+			packetMatrix, err := codec.DecodeJitNetPacketMatrix(r)
+			if err != nil {
+				panic(err)
+			}
+			data := make([]byte, packetMatrix.Datasize)
+			_, err = io.ReadFull(r, data)
+			if err != nil {
+				panic(errors.Wrap(err, "failed to read data"))
+			}
+			// TODO: Handle
+		case codec.JitMatrixLatencyPacketID:
+			// TODO: Handle
+		case codec.JitMessagePacketID:
+			// TODO: Handle
+		default:
+			// TODO: Log Warning
+		}
+	}
 }
